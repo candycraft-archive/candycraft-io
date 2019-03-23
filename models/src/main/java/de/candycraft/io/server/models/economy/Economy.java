f049fb3ceddaa5f417ff16925f5f91291d4a12b5
Moritz Lehmann <codeexceptionde@gmail.com>
2019-03-23 13:01:59 +0100

    TD-37

---
+++ models/src/main/java/de/candycraft/io/server/models/economy/Economy.java
@@ -0,0 +1,17 @@
+package de.candycraft.io.server.models.economy;
+
+import de.candycraft.io.server.models.IOModel;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder(builderClassName = "Builder")
+public class Economy extends IOModel {
+
+    @Getter
+    private String name;
+
+    @Getter
+    private String characters;
+
+
+}
