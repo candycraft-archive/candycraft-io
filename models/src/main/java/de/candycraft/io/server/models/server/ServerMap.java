1062b036a708c8adfd9de144cb11e70f888a8fcb
marvinerkes <marvin@erkes.me>
2019-03-23 14:07:34 +0100

    [TD-42] Implemented server&server type&server map model

--- models/src/main/java/de/candycraft/io/server/models/server/ServerMap.java
+++ models/src/main/java/de/candycraft/io/server/models/server/ServerMap.java
@@ -7,18 +7,12 @@ import lombok.Getter;
  * Created by Marvin Erkes on 2019-03-23.
  */
 @Builder(builderClassName = "Builder")
-public class Server {
+public class ServerMap {
 
     @Getter
     private int id;
     @Getter
-    private String name;
-    @Getter
     private int serverType;
     @Getter
-    private int players;
-    @Getter
-    private int maxPlayers;
-    @Getter
-    private double tps;
+    private String name;
 }
--- models/src/main/java/de/candycraft/io/server/models/server/ServerType.java
+++ models/src/main/java/de/candycraft/io/server/models/server/ServerType.java
@@ -1,9 +1,28 @@
 package de.candycraft.io.server.models.server;
 
+import lombok.Builder;
+import lombok.Getter;
+
 /**
  * Created by Marvin Erkes on 2019-03-23.
  */
+@Builder(builderClassName = "Builder")
 public class ServerType {
 
-
+    @Getter
+    private String id;
+    @Getter
+    private String name;
+    @Getter
+    private int minimumServers;
+    @Getter
+    private int maxServers;
+    @Getter
+    private int availableServers;
+    @Getter
+    private int reservedRAM;
+    @Getter
+    private int maxRAM;
+    @Getter
+    private int maxPlayers;
 }
