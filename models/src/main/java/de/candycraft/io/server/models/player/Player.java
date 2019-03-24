0d9838a93df356d960cde0624e57e1e573004855
marvinerkes <marvin@erkes.me>
2019-03-24 21:30:02 +0100

    Fixed player creation

--- models/src/main/java/de/candycraft/io/server/models/player/Player.java
+++ models/src/main/java/de/candycraft/io/server/models/player/Player.java
@@ -3,6 +3,7 @@ package de.candycraft.io.server.models.player;
 import de.candycraft.io.server.models.IOModel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
 
@@ -13,6 +14,7 @@ import java.util.UUID;
 public class Player extends IOModel {
 
     @Getter
+    @Setter
     private int id;
     @Getter
     private UUID uuid;
